b5f6572b4e83ec0aa79f1b932af287c2309730ea
Mike McGrath <mike@dopefruit.com>
2019-09-13 20:06:32 -0400

    bump

--- serverless.js
+++ serverless.js
@@ -1,11 +1,22 @@
 const { Component } = require('@serverless/core');
-const aws = require('aws-sdk');
+const lambda = require('aws-sdk/clients/lambda');
 
 class Mcgrath extends Component {
+
+	/**
+	 * This method is called when the serverless comands is run
+	 * @param {object} inputs 
+	 */
 	async default(inputs = {}) {
-		console.log(this.context)
+		console.log(this.context.credentials);
+
+		const $L = new lambda({
+			apiVersion: '2015-03-31',
+			credentials: this.context.credentials.aws
+		});
 	}
 }
 
 
 module.exports = Mcgrath;
+
